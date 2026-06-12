497151b8f8db6f2822b9820b0195ddbf95148b9a
David19 <davidleonel911@gmail.com>
2026-06-11 20:14:41 -0500

    contador

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,11 @@
             labelone = new Label();
             button1 = new Button();
             txtNombre = new TextBox();
+            button2 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            txtNumero = new Label();
+            lblNumero = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // labelone
@@ -46,7 +51,7 @@
             // button1
             // 
             button1.BackColor = Color.FromArgb(192, 255, 255);
-            button1.Location = new Point(348, 221);
+            button1.Location = new Point(403, 65);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 1;
@@ -63,17 +68,59 @@
             txtNombre.TabIndex = 2;
             txtNombre.TextChanged += textBox1_TextChanged;
             // 
+            // button2
+            // 
+            button2.Location = new Point(420, 223);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "aumentar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(616, 194);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 4;
+            numericUpDown1.Value = new decimal(new int[] { 11, 0, 0, 0 });
+            // 
+            // txtNumero
+            // 
+            txtNumero.AutoSize = true;
+            txtNumero.Location = new Point(338, 281);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(17, 20);
+            txtNumero.TabIndex = 5;
+            txtNumero.Text = "0";
+            txtNumero.Click += txtNumero_Click;
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(338, 232);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(50, 20);
+            lblNumero.TabIndex = 6;
+            lblNumero.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblNumero);
+            Controls.Add(txtNumero);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button2);
             Controls.Add(txtNombre);
             Controls.Add(button1);
             Controls.Add(labelone);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,5 +130,9 @@
         private Label labelone;
         private Button button1;
         private TextBox txtNombre;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private Label txtNumero;
+        private Label lblNumero;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ namespace WF_Project1
 {
     public partial class Form1 : Form
     {
+        private int _clickCount = 0; // Variable para contar los clics
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +10,7 @@ namespace WF_Project1
 
         private void label1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,8 +28,19 @@ namespace WF_Project1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _clickCount++; // Incrementamos el contador de clics
+            txtNumero.Text = _clickCount.ToString(); // 
+            lblNumero.Text = _clickCount.ToString(); // Actualizamos el texto, usamos Tostring porque label no admite enteros solo txt
+        }
+
+        private void txtNumero_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
